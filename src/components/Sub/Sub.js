f757d06b0480d5ad294519dd382e1b79828df02c
swapnilRahangdale <rahangdaleswapnil7030@gmail.com>
2023-05-18 13:15:22 +0530

    3rd commit

--- src/components/Sub/Sub.js
+++ src/components/Sub/Sub.js
@@ -3,7 +3,7 @@ const Sub = (abc) => {
     return(
 
         <div>
-            <p>This is a Sub Component 1</p>
+            <p>This is a Sub Component 3</p>
             <p>Substraction = {parseInt(abc.s)-parseInt(abc.t)}</p>
         </div>
 
