837666bc7e566dd7fa2bdc15399b075031c9114b
swapnilRahangdale <rahangdaleswapnil7030@gmail.com>
2023-05-18 15:36:51 +0530

    6th change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Sub from './components/Sub/Sub';
 function App() {
   return (
     <div>
-    <h1>First React Class 3</h1>
+    <h1>First React Class 5</h1>
     <p>swapnil</p>
     <Add a = '5' b = '3'></Add>                
     <Sub s = '8' t='3'/>
