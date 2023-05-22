878200d4c2aac3ae2b83e10866cd59b8862e0dc7
swapnilRahangdale <rahangdaleswapnil7030@gmail.com>
2023-05-22 18:29:43 +0530

    2nd change

--- src/components/Cal/Cal.js
+++ src/components/Cal/Cal.js
@@ -1,19 +1,23 @@
+import {useState} from "react";
 import CalWrapper from "../CalWrapper/CalWrapper";
+
 const Cal = () => {
+
+  const [inputVal1, setInputVal1] = useState(0);
 
     const allBtArr = [
         
-        {value : 0, btFun: () => {console.log(0)}},
-        {value : 1, btFun: () => {console.log(1)}},
-        {value : 2, btFun: () => {console.log(2)}},
-        {value : 3, btFun: () => {console.log(3)}},
-        {value : 4, btFun: () => {console.log(4)}},
-        {value : 5, btFun: () => {console.log(5)}}
+        {value : 0, btFun: () => { setInputVal1(0)}},
+        {value : 1, btFun: () => {setInputVal1(1)}},
+        {value : 2, btFun: () => {setInputVal1(2)}},
+        {value : 3, btFun: () => {setInputVal1(3)}},
+        {value : 4, btFun: () => {setInputVal1(4)}},
+        {value : 5, btFun: () => {setInputVal1(5)}}
     ];
 
   return (
     <div>
-      <p>This is calculator</p>
+      <p>This is calculator {inputVal1}</p>
         <CalWrapper allBtArr = {allBtArr}/>
     </div>
   );
